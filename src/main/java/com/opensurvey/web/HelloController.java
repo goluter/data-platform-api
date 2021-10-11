34ccf3e69ecace8a3d971e24bdddbe50f9508f65
nbmyng <77419986+nbmyng@users.noreply.github.com>
2021-10-11 23:13:23 +0900

    Add files via upload

---
+++ src/main/java/com/opensurvey/web/HelloController.java
@@ -0,0 +1,19 @@
+package com.opensurvey.web;
+import com.opensurvey.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
