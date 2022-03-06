4cd444692d7f0f1d78448de8d9f60a03622543f4
kang juyoung <karjyk@gmail.com>
2022-03-07 01:31:25 +0900

    chore: API doc 설정

--- src/main/java/com/govey/web/SurveyController.java
+++ src/main/java/com/govey/web/SurveyController.java
@@ -3,34 +3,54 @@ package com.govey.web;
 import com.govey.domain.survey.Survey;
 import com.govey.service.SurveyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// @CrossOrigin
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(path = "api/v1/survey")
 public class SurveyController {
-
     private final SurveyService surveyService;
 
     @GetMapping
     public List<Survey> getSurvey(){
+//        List<Survey> list = surveyService.getSurvey();
+//        // todo reponse
+//        List<SurveyResponse> listA = list.stream().map(SurveyResponse::new)
+//                .collect(Collectors.toList());
         return surveyService.getSurvey();
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Survey> retrieve(){
+        return ResponseEntity.ok(surveyService.retrieve(1L));
+    }
+
     @PostMapping
-    public void registerNewSurvey(@RequestBody Survey survey) {
+    public ResponseEntity<Survey> registerNewSurvey(@RequestBody Survey survey) {
         surveyService.addNewSurvey(survey);
+
+        return ResponseEntity.badRequest().build();
     }
-    @DeleteMapping(path = "{surveyId}")
-    public void deleteSurvey(@PathVariable("surveyId") Long surveyId) {
-        surveyService.deleteSurvey(surveyId);
-    }
+
+    // TODO request Body 로 Req 객체
     @PutMapping(path = "{surveyId}")
-    public void updateSurvey(
+    public ResponseEntity<?> updateSurvey(
             @PathVariable("surveyId") Long studentId,
             @RequestParam(required = false) String title,
             @RequestParam(required = false) String author) {
         surveyService.updateSurvey(studentId, title, author);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(path = "{surveyId}")
+    public ResponseEntity<?> deleteSurvey(@PathVariable("surveyId") Long surveyId) {
+        surveyService.deleteSurvey(surveyId);
+
+        return ResponseEntity.ok().build();
     }
 }
