3da972b859d35028e8e0c864069504abee75e0b1
nbmyng <nbmyng@gmail.com>
2022-05-01 23:20:50 +0900

    ReDoc

--- src/main/java/com/govey/config/SwaggerConfig.java
+++ src/main/java/com/govey/config/SwaggerConfig.java
@@ -3,15 +3,18 @@ package com.govey.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableWebMvc
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket swaggerAPI(){
@@ -29,5 +32,22 @@ public class SwaggerConfig {
                 .description("swagger")
                 .version("1.0")
                 .build();
+    }
+}
+
+@Configuration
+@EnableWebMvc
+class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/*.worker.js").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/");
+        // index.html 파일 경로
+        registry.addResourceHandler("/dist/**").addResourceLocations("classpath:/dist/");
+
     }
 }
